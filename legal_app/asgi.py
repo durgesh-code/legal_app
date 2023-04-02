4c5edcf74a47070e42d0d5ddaef53125411bda6e
durgeshy <durgesh.yadav@tekmindz.com>
2023-04-02 15:46:35 +0530

    delete unused file

--- legal_app/asgi.py
+++ legal_app/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for legel_app project.
+ASGI config for legal_app project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legal_app.settings')
 
 application = get_asgi_application()
--- legal_app/wsgi.py
+++ legal_app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for legel_app project.
+WSGI config for legal_app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legel_app.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'legal_app.settings')
 
 application = get_wsgi_application()
